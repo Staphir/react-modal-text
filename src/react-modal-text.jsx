aa9644a0e80b01d63c801da9f5995881fdd1bf5b
Maxime Dulieu <staphir@gmail.com>
2024-06-21 11:32:43 +0200

    paste in this project modal text component files

---
+++ src/react-modal-text.jsx
@@ -0,0 +1,29 @@
+import { useRef } from "react";
+import "./react-modal-text.css";
+
+function ModalText ({buttonName, text, onOpen, onClose}) {
+    const dialogRef = useRef();
+
+    const openModal = () => {
+        dialogRef.current.showModal();
+        onOpen && onOpen();
+    }
+    const closeModal = () => {
+        dialogRef.current.close();
+        onClose && onClose();
+    }
+
+    return (
+        <>
+            <button className="modal-button" onClick={openModal}>{buttonName}</button>
+            <dialog className="modal-dialog" ref={dialogRef}>
+                <p className="modal-text">{text}</p>
+                <div className="modal-close-button" onClick={closeModal}>
+                    <span className="modal-close-cross"/>
+                </div>
+            </dialog>
+        </>
+    )
+}
+
+export default ModalText;
