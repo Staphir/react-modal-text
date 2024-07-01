325282a4034fb87dd67dfa61b6d18673df67f247
Maxime Dulieu <staphir@gmail.com>
2024-07-01 13:05:11 +0200

    add options to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/react-modal-text.jsx'),
+      name: 'react-modal-text',
+      fileName: (format) => `react-modal-text.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
   plugins: [react()],
 })
